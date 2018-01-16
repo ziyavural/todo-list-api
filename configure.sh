2b56d05e642ced2caae3822b6aac95dd3bd8ad42
ziya.vural <ziya.vural@bilyoner.com>
2018-01-16 11:23:50 +0300

    -configure.sh, docker-compose.yml, pom.xml refactor

--- configure.sh
+++ configure.sh
@@ -8,19 +8,19 @@ set -m
 sleep 15
 
 # Setup index and memory quota
-curl -v -X POST http://couchbase_db_1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
+curl -v -X POST http://todolistapi_db_1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
 
 # Setup services
-curl -v http://couchbase_db_1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
+curl -v http://todolistapi_db_1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
 
 # Setup credentials
-curl -v http://couchbase_db_1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
+curl -v http://todolistapi_db_1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
 
 # Setup Memory Optimized Indexes
-curl -i -u Administrator:password -X POST http://couchbase_db_1:8091/settings/indexes -d 'storageMode=memory_optimized'
+curl -i -u Administrator:password -X POST http://todolistapi_db_1:8091/settings/indexes -d 'storageMode=memory_optimized'
 
 # Load travel-sample bucket
-curl -v -u Administrator:password -X POST http://couchbase_db_1:8091/sampleBuckets/install -d '["travel-sample"]'
+curl -v -u Administrator:password -X POST http://todolistapi_db_1:8091/sampleBuckets/install -d '["travel-sample"]'
 
 echo "Type: $TYPE"
 
